e73434859438ee6a67ad349ee05b017c5364d4ef
arpit2507 <38578237+arpit2507@users.noreply.github.com>
2022-09-09 15:52:00 +0530

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://13.110.210.186/CollegeMgmt/';
+$config['base_url'] = 'http://3.110.210.186/CollegeMgmt/';
 
 /*
 |--------------------------------------------------------------------------
